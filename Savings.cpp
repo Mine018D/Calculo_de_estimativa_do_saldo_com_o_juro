6ac4212489528eacca76de8f83873fba13840262
Mine018D <107138404+Mine018D@users.noreply.github.com>
2022-06-08 19:33:51 +0100

    Add files via upload

---
+++ Savings.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "savings.h"
+int main(){
+
+	float saldo, tanb;
+	printf("Insira o saldo da sua conta: ");
+	scanf("%f", &saldo);
+	printf("Insira o tanb da conta poupança: ");
+	scanf("%f", &tanb);
+	printf("%.10f\n",cp(saldo,tanb));
+	system("pause");
+return 0;
+}
